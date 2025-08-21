06943fcedf741f8c90cb3f62130baf37a8d06bc6
Airex Team <nikdjoraev0303@gmail.com>
2025-08-22 05:33:27 +0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById('menu-btn');
-const sideMenu = document.getElementById('side-menu');
-
-menuBtn.addEventListener('click', () => {
-  sideMenu.classList.toggle('active');
-});
